523bd1e4f404f255fb6dd3b2df2769a61e7f4ef0
Luiz Silva <lalsilva@gmail.com>
2023-11-26 16:24:17 -0300

    Aplicando o conceito SRP: separando a responsabilidade da API

---
+++ backend/ride/src/api.ts
@@ -0,0 +1,27 @@
+import express, { Request, Response } from 'express';
+import { getAccount, signup } from './main';
+
+export const PORT: number = 3000;
+
+const app = express();
+app.use(express.json());
+
+app.post("/signup", async function (req: Request, res: Response) {
+	try {
+		const input = req.body;
+		const output = await signup(input);
+		return res.json(output);
+	} catch (e: any) {
+		res.status(422).json({
+			message: e.message
+		});
+	}
+});
+
+app.get("/accounts/:accountId", async function (req: Request, res: Response) {
+	const accountId = req.params.accountId;
+	const output = await getAccount(accountId);
+	return res.json(output);
+});
+
+app.listen(PORT, (): void => console.log(`Server running on port ${PORT}`));
