8b825565dbd78ea10e7d4017e42ae62bc5637569
Luiz Silva <lalsilva@gmail.com>
2023-12-18 18:50:50 -0300

    Aplicação das camadas, pastas e composition root

--- backend/ride/src/main.ts
+++ backend/ride/src/main.ts
@@ -6,12 +6,21 @@ import PgPromiseAdapter from "./infra/database/PgPromiseAdapter";
 import Signup from "./application/usecase/Signup";
 import MainController from "./infra/controller/MainController";
 
+// composition root ou entry point
+// criar o grafo de dependências utilizado no projeto
+
+// framework and driver and library
 const httpServer = new ExpressAdapter();
 const databaseConnection = new PgPromiseAdapter();
+
+// interface adapter
 const accoundRepository = new AccountRepositoryDatabase(databaseConnection);
 const logger = new LoggerConsole();
+
+// use case
 const signup = new Signup(accoundRepository, logger);
 const getAccount = new GetAccount(accoundRepository);
 
+// interface adapter
 new MainController(httpServer, signup, getAccount);
 httpServer.listen(3000);
